38637ea172fd3e2c2a55ab961a0a7c33b99cccd6
Ray-Ezra <raymondezra276@gmail.com>
2024-02-11 10:31:53 +0300

    Bug fix

--- src/app/(form)/actions.js
+++ src/app/(form)/actions.js
@@ -1,54 +1,45 @@
-"use server"
-import { revalidatePath } from "next/cache"
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { revalidatePath } from "next/cache";
 
+const uploadClient = new S3Client({
+  region: process.env.NEXT_AWS_S3_REGION,
+  credentials: {
+    secretAccessKey: process.env.NEXT_AWS_S3_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.NEXT_AWS_S3_ACCESS_KEY_ID,
+  },
+});
 
-
-// NEXT_AWS_S3_BUCKET_NAME= reezy-bucket
-
-	const client = new S3Client({
-		region: process.env.NEXT_AWS_S3_REGION,
-		credentials:{
-			accessKeyId: process.env.NEXT_AWS_S3_ACCESS_KEY_ID,
-			secretAccessKey:process.env.NEXT_AWS_S3_SECRET_ACCESS_KEY
-		}
-	});
-
-	async function uploadFileToS3(file, fileName){
-		const fileBuffer = file;
-		const params = {
-			Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME,
-			Key: `${fileName}`,
-			body: fileBuffer,
-			contentType: "image/jpg"
-		}
-
-		const command = new PutObjectCommand(params)
-		try {
-			const response =await client.send(command)
-			console.log("file uploaded Succesfully", response)
-			return fileName
-		} catch (error) {
-			throw error;
-		}
-
-	}
+const uploadToS3 = async (buffer, filename) => {
+  const params = {
+    Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME,
+    Key: `${filename}`,
+    Body: buffer,
+    ContentType: "image/png", // Assuming it's a PNG file
+  };
+  const command = new PutObjectCommand(params);
+  try {
+    const response = await uploadClient.send(command);
+    console.log(response);
+    return filename;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
 
 export async function UploadFile(prevState, formData) {
-      try {
-				const file = formData.get("file")
-
-				if(file.size === 0){
-					return{ status: "error", messsage:"PPlease select file"}
-				}
-				const buffer = Buffer.from(await file.arrayBuffer());
-				await uploadFileToS3(buffer, file.name)
-
-
-
-				revalidatePath("/");
-				return {status: "success", messsage:"File upload Succeful!"}
-			} catch (error) {
-				return{status: "error", messsage:"File upload not Succeful!"}
-			}
+  try {
+    const file = formData.get("file");
+    console.log(file);
+    if (!file || file.size === 0) {
+      return { status: "error", message: "Please add a file!" };
+    }
+    const buffer = Buffer.from(file.buffer);
+    await uploadToS3(buffer, file.name);
+    revalidatePath("/");
+    return { status: "success", message: "File has been uploaded" };
+  } catch (err) {
+    console.log(err);
+    return { status: "error", message: "File has not been uploaded" };
+  }
 }
--- src/app/(form)/submitBtn.js
+++ src/app/(form)/submitBtn.js
@@ -1,12 +1,20 @@
-"use client"
-import { useFormStatus } from 'react-dom'
+"use client";
 
+import { useFormStatus } from "react-dom";
 
-export default function submitBtn(){
-    const {pending} = useFormStatus()
-    return(
-        <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" aria-disabled={pending}>
-            File Upload
-          </button>
-    )
-}
+import React from "react";
+
+export default function Submitbutton(){
+  const { pending } = useFormStatus();
+  return (
+<button
+  type="submit"
+  className={"bg-blue-500 text-white font-bold py-2 px-4 rounded"}
+  aria-disabled={pending}
+>
+  {pending ? "Uploading...." : "Upload File"}
+</button>
+
+  );
+};
+
